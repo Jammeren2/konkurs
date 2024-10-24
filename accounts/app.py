396981d15a25510fb653b62b07cbca4af38abb19
Jammeren2 <52398081+Jammeren2@users.noreply.github.com>
2024-10-24 15:53:38 +0700

    Update app.py

--- accounts/app.py
+++ accounts/app.py
@@ -257,7 +257,7 @@ def get_account():
 
 @app.route('/api/Accounts/Update', methods=['PUT'])
 @jwt_required()
-def update_account():
+def update_account11():
     current_user = get_jwt_identity()
     data = request.json
     user = find_user_by_username(current_user)
