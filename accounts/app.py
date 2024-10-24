e2e2c78945a3b55fa74603dfc28934bcb182c357
Jammeren2 <52398081+Jammeren2@users.noreply.github.com>
2024-10-24 16:50:09 +0700

    Update app.py

--- accounts/app.py
+++ accounts/app.py
@@ -428,6 +428,6 @@ def get_doctor_by_id(id):
 
 if __name__ == "__main__":
     app.run(
-        # port=8081, 
-        port=25565,
+        port=8081, 
+        # port=25565,
         host='0.0.0.0', debug=True)
